18a1dddf7a803cb4b8d95889f4902f119e03f892
hridoyrdx <smallrdx@gmail.com>
2021-02-01 21:15:08 +0600

    Button Toggle

--- src/App.js
+++ src/App.js
@@ -1,21 +1,25 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
-import Header from './components/header';
-import Sidebar from './components/sidebar';
-import Footer from './components/footer';
+// import Header from './components/header';
+// import Sidebar from './components/sidebar';
+// import Footer from './components/footer';
 import React, {useState} from 'react';
 
 const App = () => {
 
   const [name, setName] = useState('Hridoy');
   
-  changeName = () => {
-    setName('Hridoy Ahmed')
+  const changeName = () => {
+    if(name == 'Hridoy'){
+      setName('Hridoy Ahmed');
+    }else{
+      setName('Hridoy');
+    }
   }
 
   return ( 
     <>
-      <span>Name: {name}</span>
+      <span>Name: {name}</span><br />
      <button onClick = {changeName}>
        Change Name
        </button>
